a623553f5316be702e5f9244cfa16eaa63a52049
Scott Haselwood <scott.j.haselwood@gmail.com>
2014-04-24 22:25:14 -0700

    Added SASS and JS
    Added SASS and JS files to project

--- inc/header.php
+++ inc/header.php
@@ -2,51 +2,21 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Template &middot; Bootstrap</title>
+    <title>Bootstrap, from Twitter</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href="../assets/css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
+    <link href="stylesheets/bootstrap.css" rel="stylesheet">
+    <style>
       body {
-        padding-top: 20px;
-        padding-bottom: 40px;
-      }
-
-      /* Custom container */
-      .container-narrow {
-        margin: 0 auto;
-        max-width: 700px;
-      }
-      .container-narrow > hr {
-        margin: 30px 0;
-      }
-
-      /* Main marketing message and sign up button */
-      .jumbotron {
-        margin: 60px 0;
-        text-align: center;
-      }
-      .jumbotron h1 {
-        font-size: 72px;
-        line-height: 1;
-      }
-      .jumbotron .btn {
-        font-size: 21px;
-        padding: 14px 24px;
-      }
-
-      /* Supporting marketing content */
-      .marketing {
-        margin: 60px 0;
-      }
-      .marketing p + h4 {
-        margin-top: 28px;
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
       }
     </style>
-    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="stylesheets/bootstrap-responsive.css" rel="stylesheet">
+    <link href="stylesheets/screen.css" rel="stylesheet">
+    
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -63,4 +33,30 @@
 
   <body>
 
-        <div class="container-narrow">
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Project name</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li><a href="#">Home</a></li>
+              <li><a href="#about">About</a>
+                <ul class="nav-dropdown">
+                  <li><a href="#">Something</a></li>
+                  <li><a href="#">Else</a></li>
+                  <li><a href="#">Entirely</a></li>
+                </ul>
+              </li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php include('inc/header.php'); ?>
 
-<h1>Helo World</h1>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, ab, delectus, soluta recusandae pariatur vel veniam quos maxime ipsa accusamus minus laboriosam aspernatur quisquam neque amet dignissimos fugit repudiandae ea.</p>
-
-<h2>The New Camino Site</h2>
+<h1>Bootstrap starter template</h1>
+      <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
 
 <?php include('inc/footer.php'); ?>
