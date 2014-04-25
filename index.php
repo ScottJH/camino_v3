5450d3af38e12d05bdfae53e7c0239e93bb7db71
Scott Haselwood <scott.j.haselwood@gmail.com>
2014-04-24 18:04:23 -0700

    SASS and initial template
    Created styles directories for SASS and template from Twitter bootstrap

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('inc/header.php'); ?>
+
+<h1>Helo World</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, ab, delectus, soluta recusandae pariatur vel veniam quos maxime ipsa accusamus minus laboriosam aspernatur quisquam neque amet dignissimos fugit repudiandae ea.</p>
+
+<h2>The New Camino Site</h2>
+
+<?php include('inc/footer.php'); ?>
