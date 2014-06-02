71d810d94158eb3e40a0eb56d07e1bfa79b83ccc
Scott Haselwood <scott.j.haselwood@gmail.com>
2014-06-02 15:34:35 -0700

    Form edits
    Edits to form styling and layout

--- donation-form/confirm_paypal.php
+++ donation-form/confirm_paypal.php
@@ -1,6 +1,5 @@
         <h1>Thank You...</h1> 
-        ...for your generous support. 
-        <p>Your donation brings us that much closer to finishing the film.</p>
+        <p>...for your generous support.</p>
         
         <p>You will receive 2 email confirmations - one from us, and one from PayPal - which you may print for your records and tax purposes.</p>
 
